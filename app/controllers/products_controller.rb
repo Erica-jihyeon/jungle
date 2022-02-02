83975c3dc0f1eeff1323b1bbaf7b8d614da57305
Erica-jihyeon <jihyeon20@gmail.com>
2022-02-02 17:14:40 +0000

    first jungle setting

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,10 @@
 class ProductsController < ApplicationController
 
   def index
+    # uses MODEL
     @products = Product.all.order(created_at: :desc)
+    # VIEW
+    # render :index  # this is implicit
   end
 
   def show
