62af34714a82503d7d0887f38bbd8e60cc18a23e
Erica-jihyeon <jihyeon20@gmail.com>
2022-02-03 01:11:19 +0000

    add authentication for admin/dashboard and dashboard ui

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: BASIC_AUTHENTICATION_USERNAME, password: BASIC_AUTHENTICATION_PASSWORD
   def show
+    @num_products = Product.count(:all)
+    @num_categories = Category.count(:all)
+    # raise @num_products.inspect
   end
 end
